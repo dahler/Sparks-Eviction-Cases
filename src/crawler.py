f301b94488f2718513bd5024584c396c0dce5fdc
dahler <dahler@gmail.com>
2020-02-22 07:10:30 -0500

    adding crawler

--- src/crawler.py
+++ src/crawler.py
@@ -1,76 +1,36 @@
-import scrapy;
 
 
-class ExtractUrls(scrapy.Spider):
-    # This name must be unique always
-    name = "extract"
+import scrapy
 
-    # Function which will be invoked
+class MySpider(scrapy.Spider):
+
+    name = 'myspider'
+    directory = '../html/'
+
+    #allowed_domains = ['http://quotes.toqoute.com']
+
     def start_requests(self):
-        # enter the URL here
-        urls = ['http://www.indopenn.com/', ]
-
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+            'https://www.masscourts.org/eservices/search.page.3.5?x=gjqNue7Jqoq4haRZvIu12g'
+        ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(self.directory+filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
 
-        # Extra feature to get title
-        title = response.css('title::text').extract_first()
+# --- it runs without project and saves in `output.csv` ---
 
-        # Get anchor tags
-        links = response.css('a::attr(href)').extract()
+from scrapy.crawler import CrawlerProcess
 
-        for link in links:
-            yield
-            {
-                'title': title,
-                'links': link
-            }
-
-            print(link)
-            if 'aal' in link:
-                yield scrapy.Request(url=link, callback=self.parse)
-
-
-
-
-def run():
-    r = ExtractUrls();
-    r.start_requests();
-
-
-run()
-
-                    # def parse(self, response):
-#     return scrapy.FormRequest.from_response(
-#         response,
-#         formdata={'username': 'randomuser', 'password': 'topsecret'},
-#         callback=self.after_login
-#      )
-
-"""
-
-
-def after_login(self, response):
-    if "Error while logging in" in response.body:
-        self.logger.error("Login failed!")
-    else:
-        self.logger.error("Login succeeded!")
-        item = SampleItem()
-        item["quote"] = response.css(".text").extract()
-        item["author"] = response.css(".author").extract()
-        return item
-
-
-def after_login(self, response):
-    if "Error while logging in" in response.body:
-        self.logger.error("Login failed!")
-    else:
-        self.logger.error("Login succeeded!")
-        item = SampleItem()
-        item["quote"] = response.css(".text").extract()
-        item["author"] = response.css(".author").extract()
-        return item
-
-"""
+c = CrawlerProcess({
+    'USER_AGENT': 'Mozilla/5.0'
+})
+c.crawl(MySpider)
+c.start()
